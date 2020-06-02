cafc7108013005c12b96f10490034e68425f1027
Chethan Kumar <chethankumar@Chethans-MacBook-Pro.local>
2020-06-02 22:57:07 +0530

    Initialize project using Create React App

---
+++ src/client/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export class App extends React.Component{
+  render(){
+    return(<h1>Welcome to Digital App Builder</h1>);
+  }
+}
+
+export default App;
